d7a36aa6cddc5a521c911cbf0a6de3145cba038a
SaikotHouse <65276816+saikothouse@users.noreply.github.com>
2024-11-02 15:02:17 +0600

    Create Header.js

---
+++ components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="p-5">
+      <h1 className="text-4xl font-bold">My Cyberpunk Portfolio</h1>
+    </header>
+  );
+};
+
+export default Header;
